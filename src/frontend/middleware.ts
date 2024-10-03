05198af952f4aefea33abaa96e97043a71461d74
Jessica Kerr <jessitron@honeycomb.io>
2024-10-02 20:09:35 -0500

    Now there is a string from an actual logger

--- src/frontend/middleware.ts
+++ src/frontend/middleware.ts
@@ -1,11 +1,16 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { logger } from './utils/logger';
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
     console.log(`Received ${request.method} request to ${request.url} at ${new Date()}`);
+    const startTime = Date.now();
+    const requestId = request.headers.get('x-request-id');
     const response = NextResponse.next();
-    console.log(`Responding to ${request.url} status ${response.status}, ${request.headers.get('x-request-id')}`);
+    const duration = Date.now() - startTime;
+    logger.info({"http.method": request.method, "request.id": requestId }, c
+        `${request.method} ${request.url} ${response.status} ${duration}ms request_id=${requestId}`);
     return response;
 }
  
