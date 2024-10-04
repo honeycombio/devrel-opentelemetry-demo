94b7d21cac72267d885156e55defee306fd3111e
Jessica Kerr <jessitron@honeycomb.io>
2024-10-03 22:23:50 -0500

    Add otel logging

--- src/frontend/utils/telemetry/Instrumentation.js
+++ src/frontend/utils/telemetry/Instrumentation.js
@@ -1,7 +1,8 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-const opentelemetry = require('@opentelemetry/sdk-node');
+const otelsdk = require('@opentelemetry/sdk-node');
+const opentelemetry = require('@opentelemetry/api');
 const {getNodeAutoInstrumentations} = require('@opentelemetry/auto-instrumentations-node');
 const {OTLPTraceExporter} = require('@opentelemetry/exporter-trace-otlp-grpc');
 const { OTLPLogExporter } = require('@opentelemetry/exporter-logs-otlp-http');
@@ -10,9 +11,15 @@ const {containerDetector} = require('@opentelemetry/resource-detector-container'
 const {gcpDetector} = require('@opentelemetry/resource-detector-gcp');
 const {envDetector, hostDetector, osDetector, processDetector} = require('@opentelemetry/resources');
 
-const sdk = new opentelemetry.NodeSDK({
+console.log("Otel, tell me wtf you are doing dammit")
+opentelemetry.diag.setLogger(
+  new opentelemetry.DiagConsoleLogger(),
+  opentelemetry.DiagLogLevel.DEBUG
+);
+
+const sdk = new otelsdk.NodeSDK({
   traceExporter: new OTLPTraceExporter(),
-  logRecordProcessor: new opentelemetry.logs.SimpleLogRecordProcessor(new OTLPLogExporter()),
+  logRecordProcessor: new otelsdk.logs.SimpleLogRecordProcessor(new OTLPLogExporter()),
   instrumentations: [
     getNodeAutoInstrumentations({
       // disable fs instrumentation to reduce noise
