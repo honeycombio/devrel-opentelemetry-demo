832ae21872d31b5860db1410f6d47e2fa86ef822
Jessica Kerr <jessitron@honeycomb.io>
2024-10-04 12:06:38 -0500

    Prevent 'you aren't logged in' errors minutes after I asked it to deploy

--- scripts/login-acr.sh
+++ scripts/login-acr.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-eval "$(pulumi stack output --shell --cwd ./scripts)"
+# commenting out to save 3s every time I run this
+# this changes almost never
+# eval "$(pulumi stack output --shell --cwd ./scripts)"
+acrName=mainacra1e0ec0b
 
 echo "Logging into ACR $acrName"
 export ACRNAME=$acrName.azyurecr.io
