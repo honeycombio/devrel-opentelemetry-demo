534c3a03d3df220afc7a757c4715ead6567a5180
Jessica Kerr <jessitron@honeycomb.io>
2024-12-11 16:38:56 -0600

    Add bunyan logging, this mostly works
    it still doesn't set the damn severity

--- src/frontend/utils/telemetry/Instrumentation.js
+++ src/frontend/utils/telemetry/Instrumentation.js
@@ -3,13 +3,15 @@
 
 const otelsdk = require('@opentelemetry/sdk-node');
 const opentelemetry = require('@opentelemetry/api');
-const {getNodeAutoInstrumentations} = require('@opentelemetry/auto-instrumentations-node');
-const {OTLPTraceExporter} = require('@opentelemetry/exporter-trace-otlp-grpc');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
 const { OTLPLogExporter } = require('@opentelemetry/exporter-logs-otlp-http');
-const {awsEc2Detector, awsEksDetector} = require('@opentelemetry/resource-detector-aws');
-const {containerDetector} = require('@opentelemetry/resource-detector-container');
-const {gcpDetector} = require('@opentelemetry/resource-detector-gcp');
-const {envDetector, hostDetector, osDetector, processDetector} = require('@opentelemetry/resources');
+const { awsEc2Detector, awsEksDetector } = require('@opentelemetry/resource-detector-aws');
+const { containerDetector } = require('@opentelemetry/resource-detector-container');
+const { gcpDetector } = require('@opentelemetry/resource-detector-gcp');
+const { envDetector, hostDetector, osDetector, processDetector } = require('@opentelemetry/resources');
+
+const { BunyanInstrumentation } = require('@opentelemetry/instrumentation-bunyan');
 
 // console.log("Otel, tell me wtf you are doing")
 // opentelemetry.diag.setLogger(
@@ -17,7 +19,7 @@ const {envDetector, hostDetector, osDetector, processDetector} = require('@opent
 //   opentelemetry.DiagLogLevel.INFO
 // );
 
-console.log("Instrumenting node with otel");
+console.log('Instrumenting node with otel');
 const sdk = new otelsdk.NodeSDK({
   // n.b. - the service for the next.js backend is being
   // sent to Honeycomb as 'api-gateway' - this is done
@@ -25,7 +27,7 @@ const sdk = new otelsdk.NodeSDK({
   // here!
 
   traceExporter: new OTLPTraceExporter({
-    url: '/otlp-http/v1/traces'
+    url: '/otlp-http/v1/traces',
   }),
   logRecordProcessor: new otelsdk.logs.BatchLogRecordProcessor(new OTLPLogExporter()),
   instrumentations: [
@@ -37,7 +39,8 @@ const sdk = new otelsdk.NodeSDK({
       '@opentelemetry/instrumentation-http': {
         enabled: false,
       },
-    })
+    }),
+    new BunyanInstrumentation(),
   ],
   resourceDetectors: [
     containerDetector,
