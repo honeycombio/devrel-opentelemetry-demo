ef708ca1367a7e952f80737df4c5b8e1155eba70
Ken Rimple <kenrimple@honeycomb.io>
2024-12-11 09:35:32 -0500

    fixed upfront-end o11y settings, inquiring about api-gateway calls not being attached to traces

--- src/frontend/pages/_app.tsx
+++ src/frontend/pages/_app.tsx
@@ -9,11 +9,9 @@ import CartProvider from '../providers/Cart.provider';
 import { ThemeProvider } from 'styled-components';
 import Theme from '../styles/Theme';
 import FrontendTracer from '../utils/telemetry/FrontendTracer';
-import {OpenFeature, OpenFeatureProvider} from '@openfeature/react-sdk';
-import {HoneycombWebSDK} from "@honeycombio/opentelemetry-web";
-import {getWebAutoInstrumentations} from "@opentelemetry/auto-instrumentations-web";
-import SessionGateway from "../gateways/Session.gateway";
-import {FlagdWebProvider} from "@openfeature/flagd-web-provider";
+import { OpenFeature, OpenFeatureProvider } from '@openfeature/react-sdk';
+import SessionGateway from '../gateways/Session.gateway';
+import { FlagdWebProvider } from '@openfeature/flagd-web-provider';
 
 console.log(`Initializing _app 1`);
 
@@ -28,73 +26,32 @@ declare global {
   }
 }
 
-if (typeof window !== 'undefined') {
-  const configDefaults = {
-    ignoreNetworkEvents: true,
-    propagateTraceHeaderCorsUrls: [
-      /.+/g, // Regex to match your backend URLs. Update to the domains you wish to include.
-    ],
-  };
+if (typeof window  !== 'undefined' && window.location) {
+  const session = SessionGateway.getSession();
+  // Set context prior to provider init to avoid multiple http calls
+  OpenFeature.setContext({ targetingKey: session.userId, ...session }).then(() => {
+    /**
+     * We connect to flagd through the envoy proxy, straight from the browser,
+     * for this we need to know the current hostname and port.
+     */
 
-  // API key set in collector we proxy to
-  const sdk = new HoneycombWebSDK({
-    endpoint: '/otlp-http/v1/traces',
-    debug: true, // Set to false for production environment.
-    // ignores checking for things like api keys
-    skipOptionsValidation: true,
-    serviceName: 'frontend-web', // Replace with your application name. Honeycomb uses this string to find your dataset when we receive your data. When no matching dataset exists, we create a new one with this name if your API Key has the appropriate permissions.
-    instrumentations: [
-      // new LongTaskInstrumentation({
-      //   enabled: true,
-      //   observerCallback: (span) => {
-      //     span.setAttribute('location.pathname', window.location.pathname)
-      //   }
-      // }),
-      getWebAutoInstrumentations({
-        // Loads custom configuration for xml-http-request instrumentation.
-        '@opentelemetry/instrumentation-xml-http-request': configDefaults,
-        '@opentelemetry/instrumentation-fetch': configDefaults,
-        '@opentelemetry/instrumentation-document-load': configDefaults,
-        '@opentelemetry/instrumentation-user-interaction': {
-          enabled: true,
-          eventNames: ['click'], // the default, can add many more
-        },
-      }),
-    ],
+    const useTLS = window.location.protocol === 'https:';
+    let port = useTLS ? 443 : 80;
+    if (window.location.port) {
+      port = parseInt(window.location.port, 10);
+    }
+
+    OpenFeature.setProvider(
+      new FlagdWebProvider({
+        host: window.location.hostname,
+        pathPrefix: 'flagservice',
+        port: port,
+        tls: useTLS,
+        maxRetries: 3,
+        maxDelay: 10000,
+      })
+    );
   });
-  try {
-    sdk.start();
-  } catch (e) {
-    // TODO - do we use a logging API on the client?
-    console.error('Failed to start Honeycomb SDK', e);
-  }
-  if (window.location) {
-    const session = SessionGateway.getSession();
-    // Set context prior to provider init to avoid multiple http calls
-    OpenFeature.setContext({ targetingKey: session.userId, ...session }).then(() => {
-      /**
-       * We connect to flagd through the envoy proxy, straight from the browser,
-       * for this we need to know the current hostname and port.
-       */
-
-      const useTLS = window.location.protocol === 'https:';
-      let port = useTLS ? 443 : 80;
-      if (window.location.port) {
-        port = parseInt(window.location.port, 10);
-      }
-
-      OpenFeature.setProvider(
-        new FlagdWebProvider({
-          host: window.location.hostname,
-          pathPrefix: 'flagservice',
-          port: port,
-          tls: useTLS,
-          maxRetries: 3,
-          maxDelay: 10000,
-        })
-      );
-    });
-  }
 }
 
 const queryClient = new QueryClient();
