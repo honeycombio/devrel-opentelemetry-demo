7794a8f99d58648117b3a273704de8a986937512
Ken Rimple <kenrimple@honeycomb.io>
2025-05-21 12:14:46 -0400

    added v0.18.0 of HFO - but had to comment out original frontend tracer due to breaking changes.

--- src/frontend/utils/telemetry/FrontendTracer.ts
+++ src/frontend/utils/telemetry/FrontendTracer.ts
@@ -6,11 +6,11 @@ import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
 import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
-import { Resource, browserDetector } from '@opentelemetry/resources';
-import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+import { Resource } from '@opentelemetry/resources';
+import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { SessionIdProcessor } from './SessionIdProcessor';
-import { detectResourcesSync } from '@opentelemetry/resources/build/src/detect-resources';
+import { browserDetector } from '@opentelemetry/opentelemetry-browser-detector';
 
 const {
   NEXT_PUBLIC_OTEL_SERVICE_NAME = '',
@@ -19,54 +19,55 @@ const {
 } = typeof window !== 'undefined' ? window.ENV : {};
 
 const FrontendTracer = async () => {
-  const { ZoneContextManager } = await import('@opentelemetry/context-zone');
-
-  let resource = new Resource({
-    [SEMRESATTRS_SERVICE_NAME]: NEXT_PUBLIC_OTEL_SERVICE_NAME,
-  });
-  const detectedResources = detectResourcesSync({ detectors: [browserDetector] });
-  resource = resource.merge(detectedResources);
-
-  const provider = new WebTracerProvider({
-    resource,
-    spanProcessors: [
-      new SessionIdProcessor(),
-      new BatchSpanProcessor(
-        new OTLPTraceExporter({
-          url: NEXT_PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT || 'http://localhost:4318/v1/traces',
-        }),
-        {
-          scheduledDelayMillis: 500,
-        }
-      ),
-    ],
-  });
-
-  const contextManager = new ZoneContextManager();
-
-  provider.register({
-    contextManager,
-    propagator: new CompositePropagator({
-      propagators: [
-        new W3CBaggagePropagator(),
-        new W3CTraceContextPropagator()],
-    }),
-  });
-
-  registerInstrumentations({
-    tracerProvider: provider,
-    instrumentations: [
-      getWebAutoInstrumentations({
-        '@opentelemetry/instrumentation-fetch': {
-          propagateTraceHeaderCorsUrls: /.*/,
-          clearTimingResources: true,
-          applyCustomAttributesOnSpan(span) {
-            span.setAttribute('app.synthetic_request', IS_SYNTHETIC_REQUEST);
-          },
-        },
-      }),
-    ],
-  });
+  // const { ZoneContextManager } = await import('@opentelemetry/context-zone');
+  //
+  // let resource = new Resource({
+  //   [ATTR_SERVICE_NAME]: NEXT_PUBLIC_OTEL_SERVICE_NAME,
+  // });
+  // const detectedResources = detectResourcesSync({ detectors: [browserDetector] });
+  // resource = resource.merge(detectedResources);
+  //
+  // const provider = new WebTracerProvider({
+  //   resource,
+  //   spanProcessors: [
+  //     new SessionIdProcessor(),
+  //     new BatchSpanProcessor(
+  //       new OTLPTraceExporter({
+  //         url: NEXT_PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT || 'http://localhost:4318/v1/traces',
+  //       }),
+  //       {
+  //         scheduledDelayMillis: 500,
+  //       }
+  //     ),
+  //   ],
+  // });
+  //
+  // const contextManager = new ZoneContextManager();
+  //
+  // provider.register({
+  //   contextManager,
+  //   propagator: new CompositePropagator({
+  //     propagators: [
+  //       new W3CBaggagePropagator(),
+  //       new W3CTraceContextPropagator()],
+  //   }),
+  // });
+  //
+  // registerInstrumentations({
+  //   tracerProvider: provider,
+  //   instrumentations: [
+  //     getWebAutoInstrumentations({
+  //       '@opentelemetry/instrumentation-fetch': {
+  //         propagateTraceHeaderCorsUrls: /.*/,
+  //         clearTimingResources: true,
+  //         applyCustomAttributesOnSpan(span) {
+  //           span.setAttribute('app.synthetic_request', IS_SYNTHETIC_REQUEST);
+  //         },
+  //       },
+  //     }),
+  //   ],
+  // });
+  return null;
 };
 
 export default FrontendTracer;
