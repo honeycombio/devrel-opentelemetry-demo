06fee96d6580ea4b8f0e39cf59bcfe4a3bf12af9
Ken Rimple <kenrimple@honeycomb.io>
2025-11-09 11:48:25 -0500

    fixed icon in product detail button

--- src/frontend/pages/product/[productId]/index.tsx
+++ src/frontend/pages/product/[productId]/index.tsx
@@ -96,7 +96,7 @@ const ProductDetail: NextPage = () => {
                 ))}
               </Select>
               <S.AddToCart data-cy={CypressFields.ProductAddToCart} onClick={onAddItem}>
-                <Image src="/icons/Cart.svg" height="15" width="15" alt="cart" /> Add To Cart
+                <Image src="/icons/CartIcon.svg" height="15" width="15" alt="cart" /> Add To Cart
               </S.AddToCart>
             </S.Details>
           </S.Container>
